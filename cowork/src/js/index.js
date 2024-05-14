da4524029710efe2dca7dc03bb51d706ec013323
yllaciarbegla <yllaciarbegla@gmail.com>
2024-05-14 16:38:50 +0300

    faq section

--- cowork/src/js/index.js
+++ cowork/src/js/index.js
@@ -1,5 +1,5 @@
 import * as webpCheck from './modules/webp-check.js'
-// import './modules/accordion.js';
+import './modules/accordion.js';
 import './modules/header.js';
 import './modules/conveyor.js';
 
