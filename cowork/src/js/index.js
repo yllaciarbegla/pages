3e674e86be89683f283b663c01ca425b011651af
yllaciarbegla <yllaciarbegla@gmail.com>
2024-05-16 19:02:33 +0300

    end home page

--- cowork/src/js/index.js
+++ cowork/src/js/index.js
@@ -1,8 +1,9 @@
 import * as webpCheck from './modules/webp-check.js'
-import './modules/accordion.js';
 import './modules/header.js';
 import './modules/conveyor.js';
 import './modules/slider.js';
+import './modules/accordion.js';
+import './modules/modal.js';
 
 webpCheck.isWebp();
 
