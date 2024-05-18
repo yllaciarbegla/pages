4b4af3429e469eace037e8c28665656944500d97
yllaciarbegla <yllaciarbegla@gmail.com>
2024-05-18 14:18:30 +0300

    article page

--- cowork/gulp/config/path.js
+++ cowork/gulp/config/path.js
@@ -15,7 +15,7 @@ export const path = {
 	},
 	src: {
 		html: `${srcFolder}/*.html`,
-		scss: `${srcFolder}/scss/{style,home,about,contact,pricing,blog}.scss`,
+		scss: `${srcFolder}/scss/{style,home,about,contact,article,pricing,blog}.scss`,
 		js: `${srcFolder}/js/*.js`,
 		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
 		svg: `${srcFolder}/img/**/*.svg`,
