1a9f2576d9155f55318b72e525d35cdf981ef881
yllaciarbegla <yllaciarbegla@gmail.com>
2024-05-30 19:10:07 +0300

    Theme switch realization

--- portfolio 3.0/src/js/index.js
+++ portfolio 3.0/src/js/index.js
@@ -27,14 +27,6 @@ toTopButton.addEventListener('click', () => {
 	window.scrollTo(0, 0);
 });
 
-// Theme switch
-
-const themeToggler = document.querySelector('.theme-toggler');
-
-themeToggler.addEventListener('click', (e) => {
-	e.target.classList.toggle('theme-toggler_dark');
-});
-
 // Page offset for footer
 
 const pageBottom = document.querySelector('.page__bottom');
@@ -46,3 +38,88 @@ function offsetSize() {
 
 offsetSize();
 window.addEventListener('resize', offsetSize);
+
+// Theme switch
+
+const themeToggler = document.querySelector('.theme-switch__button');
+const themeMenu = document.querySelector('.theme-switch');
+const themeList = document.querySelector('.theme-list');
+const storageKey = 'theme-preference';
+
+const openThemeMenu = () => {
+	themeMenu.classList.add('theme-switch_open');
+	setTimeout(() => {
+		themeList.classList.add('theme-switch__list_visible');
+	}, 400);
+};
+
+const closeThemeMenu = () => {
+	themeList.classList.remove('theme-switch__list_visible');
+	themeMenu.classList.remove('theme-switch_open');
+};
+
+themeToggler.addEventListener('click', openThemeMenu);
+
+const getThemePreference = () => {
+	if (localStorage.getItem(storageKey)) {
+		return localStorage.getItem(storageKey);
+	} else {
+		return window.matchMedia('(prefers-color-scheme: dark)').matches
+			? 'dark'
+			: 'light';
+	}
+};
+
+const theme = {
+	value: getThemePreference(),
+};
+
+const setThemeClass = () => {
+	let currentThemeClass = `page_${document.documentElement.dataset.theme}`;
+	let newThemeClass = `page_${theme.value}`;
+
+	if (document.querySelector('.page').classList.contains('page_light')) {
+		currentThemeClass = 'page_light';
+	} else if (document.querySelector('.page').classList.contains('page_dark')) {
+		currentThemeClass = 'page_dark';
+	} else if (
+		document.querySelector('.page').classList.contains('page_monochrome')
+	) {
+		currentThemeClass = 'page_monochrome';
+	}
+
+	if (
+		document.querySelector('.page').classList.contains(`${currentThemeClass}`)
+	)
+		document.querySelector('.page').classList.remove(`${currentThemeClass}`);
+
+	document.querySelector('.page').classList.add(`${newThemeClass}`);
+};
+
+const reflectPreference = () => {
+	document.firstElementChild.setAttribute('data-theme', theme.value);
+
+	setThemeClass();
+};
+
+const setPreference = () => {
+	localStorage.setItem(storageKey, theme.value);
+	reflectPreference();
+};
+
+const switchTheme = (newTheme) => {
+	theme.value = newTheme;
+
+	setPreference();
+};
+
+window.addEventListener('load', () => {
+	reflectPreference();
+
+	themeList.addEventListener('click', (e) => {
+		if (e.target.classList.contains('theme-list__button')) {
+			switchTheme(e.target.dataset.theme);
+			closeThemeMenu();
+		}
+	});
+});
